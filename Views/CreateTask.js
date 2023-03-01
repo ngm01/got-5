76b223fd361a980e4386df015a68c77b6190ff8b
ngm01 <nathaniel.g.moore@gmail.com>
2023-03-01 17:21:38 -0500

    began work on adding tasks

--- Views/CreateTask.js
+++ Views/CreateTask.js
@@ -1,6 +1,17 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+/*
+    {
+        
+    }
+*/
 
 export default function CreateTask() {
+
+    function addTask() {
+
+    }
+
     return (
         <View>
             <Text>What do you want to call this task?</Text>
@@ -13,6 +24,25 @@ export default function CreateTask() {
             placeholder="Enter a task..." 
             />
             <Text>How long will this task take?</Text>
+            <div>
+            <input type="number" id="time" min="1" />
+            </div>
+            <Text>How often should this task be performed?</Text>
+            <div>
+                <select id="cadence">
+                    <option value="daily">daily</option>
+                    <option value="weekly">weekly</option>
+                    <option value="biweekly">twice a week</option>
+                    <option value="monthly">monthly</option>
+                    <option value="bimonthly">twice a month</option>
+                    <option value="once">one time only</option>
+                </select>
+            </div>
+            <Button
+                title="Add Task"
+                color="#fb4d3d"
+                onPress={addTask}
+             />
         </View>
     )
 }
--- Views/TaskList.js
+++ Views/TaskList.js
@@ -1,9 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function TaskList() {
+
+    const fakeData = [
+        {
+            time: 1,
+            title: 'start laundry',
+            lastPerformed: null,
+            cadence: 'weekly',
+            created: null,
+            tags: []
+        },
+        {
+            time: 15,
+            title: 'do the dishes',
+            lastPerformed: null,
+            cadence: 'daily',
+            created: null,
+            tags: []
+        },
+        {
+            time: 15,
+            title: 'vacuum living room',
+            lastPerformed: null,
+            cadence: 'bimonthly',
+            created: null,
+            tags: []
+        },
+    ]
+
     return (
          <View>
-            <Text>Task list goes here...</Text>
+            <FlatList data={fakeData}
+                renderItem={({item}) => <Text>{item.title}</Text>} />
         </View>
     )
 }
