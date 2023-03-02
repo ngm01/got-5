7a0c626331127c5f4efca944e8343628e971d5ee
ngm01 <nathaniel.g.moore@gmail.com>
2023-03-02 00:46:10 -0500

    continued work on task creator, added dropdown menu library

--- Views/CreateTask.js
+++ Views/CreateTask.js
@@ -1,5 +1,7 @@
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState } from 'react';
 /*
     {
         
@@ -8,8 +10,22 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function CreateTask() {
 
-    function addTask() {
+    const [taskTitle, setTaskTitle] = useState('yo');
+    const [taskTime, setTaskTime] = useState('');
 
+    const [open, setOpen] = useState(false);
+    const [taskCadence, setTaskCadence] = useState('weekly');
+    const [items, setItems] = useState([
+      {label: 'Daily', value: 'daily'},
+      {label: 'Weekly', value: 'weekly'},
+      {label: 'Every other week', value: 'biweekly'},
+      {label: 'Monthly', value: 'monthly'},
+      {label: 'Only once', value: 'once'}
+    ]);
+  
+
+    function addTask() {
+        alert(`added task: ${taskTitle}, which will take ${taskTime} minutes, to be performed ${taskCadence}`);
     }
 
     return (
@@ -21,23 +37,29 @@ export default function CreateTask() {
             borderColor: 'gray',
             borderWidth: 1,
             }}
+            onChangeText={setTaskTitle}
+            value={taskTitle}
             placeholder="Enter a task..." 
             />
-            <Text>How long will this task take?</Text>
-            <div>
-            <input type="number" id="time" min="1" />
-            </div>
-            <Text>How often should this task be performed?</Text>
-            <div>
-                <select id="cadence">
-                    <option value="daily">daily</option>
-                    <option value="weekly">weekly</option>
-                    <option value="biweekly">twice a week</option>
-                    <option value="monthly">monthly</option>
-                    <option value="bimonthly">twice a month</option>
-                    <option value="once">one time only</option>
-                </select>
-            </div>
+            <Text>How many minutes will this task take?</Text>
+            <TextInput             
+                style={{
+                height: 40,
+                borderColor: 'gray',
+                borderWidth: 1,
+                }}
+                onChangeText={setTaskTime} 
+                keyboardType="numeric" />
+            
+            <Text>How often should this task be performed? ℹ️</Text>
+            <DropDownPicker
+                open={open}
+                value={taskCadence}
+                items={items}
+                setOpen={setOpen}
+                setValue={setTaskCadence}
+                setItems={setItems}
+             />
             <Button
                 title="Add Task"
                 color="#fb4d3d"
